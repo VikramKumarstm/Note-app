d5d553a3be4573155748c99a4d743a5a02f91b6c
VikramKumarstm <vikramclg0000@gmail.com>
2025-11-30 14:57:52 +0530

    create route

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import 'dotenv/config'
 import { connectDatabase } from './config/databaseConfig.js';
+import userRoute from './routes/userRoute.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000
 
-//database connection
-connectDatabase();
+//http://localhost:8080/user/register
+app.use('/user', userRoute)
 
 app.listen(PORT, () => {
+    //database connection
+    connectDatabase();  
     console.log(`server is listening at localhost:${PORT}`);
 })
